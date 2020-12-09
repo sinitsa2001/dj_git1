bfa0da69e96da4e28a3f2705be4661836ed0422c
Sergey Sinitsyn <sinitsa2001@mail.ru>
2020-12-09 15:10:48 +0300

    Заливаю изменени 2

---
+++ mainapp/views.py
@@ -0,0 +1,39 @@
+from django.shortcuts import render
+
+def index (request):
+    context = {
+        'title': 'Ты в начале пути',
+    }
+    return render(request, 'mainapp/index.html', context)
+
+def products (request):
+    context = {
+        'title': 'Эти вещи для тебя',
+        'products': [
+            { 'name':'Худи черного цвета с монограммами adidas Originals', 'price':'6 090,00 руб.'},
+            {'name': 'Синяя куртка The North Face', 'price': '23 725,00 руб.'},
+            {'name': 'Коричневый спортивный oversized-топ ASOS DESIGN', 'price': '3 390,00 руб.'},
+            {'name': 'Черный рюкзак Nike Heritage', 'price': '2 340,00 руб.'},
+            {'name': 'Черные туфли на платформе с 3 парами люверсов Dr Martens 1461 Bex', 'price': '13 590,00 руб.'},
+            {'name': 'Темно-синие широкие строгие брюки ASOS DESIGN', 'price': '2 890,00 руб.'},
+        ]
+    }
+    return render(request, 'mainapp/products.html', context)
+
+
+def test_context (request):
+    context ={
+        'title':'добро пожаловать',
+        'username':'Lele Sinitsyna',
+        'products': [
+            {'name':'Черное худи', 'price': '12000 usd'},
+            {'name': 'Белое разное', 'price': '2000 usd'},
+        ],
+        # 'promotion':True,
+        'promotion_products':[
+            {'name': 'Тухольки', 'price': 'Дешевле грибов'},
+        ]
+
+    }
+# products= context['products']
+    return render(request, 'mainapp/context.html',context)
