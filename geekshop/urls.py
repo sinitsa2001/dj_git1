d631a0710a0e11c80d54dee000ca5f2656d67a34
Sergey Sinitsyn <sinitsa2001@mail.ru>
2020-12-11 23:52:32 +0300

    ДЗ № 3

--- geekshop/urls.py
+++ geekshop/urls.py
@@ -16,10 +16,16 @@ Including another URLconf
 from django.contrib import admin
 from django.urls import path
 from mainapp import views as mainapp_views
+from django.conf import settings
+from django.conf.urls.static import static
 
 urlpatterns = [
     path('admin/', admin.site.urls),
-    path('', mainapp_views.index),
-    path('products/', mainapp_views.products),
+    path('', mainapp_views.index,name ='index'),
+    path('products/', mainapp_views.products, name ='products'),
     path('test_context/',mainapp_views.test_context),
 ]
+
+if settings.DEBUG:
+    urlpatterns += static(settings.MEDIA_URL, document_root = settings.MEDIA_ROOT)
+
